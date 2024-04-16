94edfc17ee0d5fd469c55f73994ad256762cb4c3
RUCHIKA YADAV <155508374+ruchika3w@users.noreply.github.com>
2024-04-16 15:25:46 +0530

    Update Home.jsx

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import './home.css'; // Import your CSS file
 
 function Home() {
-  return (
-    <div>
-      Home <br></br>
-      <Link to = "/login" >Login</Link>
-      <br></br>
-      <Link to = "/register" >Register</Link>
+  useEffect(() => {
+    // Add 'show' class to trigger animations when component mounts
+    document.querySelector('.home-container').classList.add('show');
+  }, []);
 
+  return (
+    <div className="home-container">
+      <div className="background-animation"></div>
+      <h1>Welcome to Our Website</h1>
+      <p className="description">NETWORKING - BINARY MLM 🔥</p>
+      <div className="buttons">
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
